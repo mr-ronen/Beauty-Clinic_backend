ea0ea3cadb2da729bf01de98609705d596484337
mr-ronen <112888529+mr-ronen@users.noreply.github.com>
2024-01-20 11:08:59 +0200

    fixed migration!!!!

--- BeautyClinicApi/Models/Order.cs
+++ BeautyClinicApi/Models/Order.cs
@@ -4,14 +4,13 @@
     {
         public int OrderId { get; set; }
         public int UserId { get; set; }
-        public User User { get; set; }
+        public User? User { get; set; }
         public DateTime OrderDate { get; set; }
         public decimal TotalAmount { get; set; }
-        public string Status { get; set; }
+        public string? Status { get; set; }
         public int OrderNumber { get; set; }
 
+        public ICollection<OrderDetail> OrderDetails { get; set; }
 
-        // Add a property for a single OrderDetail
-        public OrderDetail OrderDetail { get; set; }
     }
 }
